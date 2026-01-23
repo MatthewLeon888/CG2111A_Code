fe678b1921e143cffc5ada5ea67c2381ef5723d4
Yi An <yiannie726@gmail.com>
2026-01-23 16:44:52 +0800

    push code

--- Studio 3/p2_2/p2_2.ino
+++ Studio 3/p2_2/p2_2.ino
@@ -2,6 +2,8 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+static volatile bool A_triggered = false;
+
 //int count = 0;
 // static volatile unsigned long _timerTicks = 0;// The _timerTicks variable maintains a count of how many 100us ticks have passed by.
 // // Our last time and current time variables
@@ -15,12 +17,15 @@ void setupTimer() {
   // Set Initial Timer value
   
   // Set CTC mode and make toggle PD6/OC0A pin on compare match
-  TCCR1A = 0b00000000;
+  TCCR1A = 0b00000010;
+  TCCR1B = 0b00000000;
   // Enable interrupts.
-  TIMSK1 |= 0b110;
+  TIMSK1 = 0b110;
   TCNT1 = 0;
+
   // Place TOP timer value to Output compare register
   OCR1A = 39999;
+  OCR1B = 2999;
 }
 
 // Timer start function
@@ -34,11 +39,14 @@ void startTimer() {
 ISR(TIMER1_COMPA_vect) {
   // When triggered, set servo pin to HIGH
   PORTD |= 0b00000010;
+  A_triggered = true;
 }
 
 ISR(TIMER1_COMPB_vect) {
-  PORTD &= 0b11111101;
-
+  if (A_triggered) {
+    PORTD &= 0b11111101;
+    A_triggered = false;
+  }
 }
 
 void setup() {
