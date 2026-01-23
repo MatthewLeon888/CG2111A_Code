34a3a7662f173ac1ff5ab00115b6414cc84d254e
MatthewLeon888 <matthew.leon.888@gmail.com>
2026-01-23 14:23:39 +0800

    Update p0

--- Studio 3/p0/p0.ino
+++ Studio 3/p0/p0.ino
@@ -6,21 +6,21 @@
 
 // This variable decides which LED's turn it is to flash.
 // 0 = green, 1 = red
-static volatile int turn=0;
+static volatile int turn = 0;
 
-static unsigned long lastTime=0, currTime;
+static unsigned long lastTime = 0, currTime;
 #define THRESHOLD    10
 
 void pinISR() {
-  currTime=millis();
+  currTime = millis(); // millis() uses Timer0 behind the scenes
 
-  //debouncing
   if(currTime - lastTime > THRESHOLD) {
     // Toggles turn
     turn = 1 - turn;
     lastTime = currTime;
   }
 }
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -32,15 +32,13 @@ void setup() {
 
 void flashGreen() {
   int counter=1;
-
-  while(turn==0) {
-    for(int i=0; i<counter && turn==0; i++) {
+  while(turn == 0) {
+    for(int i = 0; i < counter && turn == 0; i++) {
       digitalWrite(GREENPIN, HIGH);
       delay(LED_DELAY);
       digitalWrite(GREENPIN, LOW);
       delay(LED_DELAY);
     }
-
     counter++;
     delay(1000);
   }
@@ -48,21 +46,21 @@ void flashGreen() {
 
 void flashRed() {
   int counter=1;
-
-  while(turn==1) {
-    for(int i=0; i<counter && turn==1; i++) {
+  while(turn == 1) {
+    for(int i = 0; i < counter && turn == 1; i++) {
       digitalWrite(REDPIN, HIGH);
       delay(LED_DELAY);
       digitalWrite(REDPIN, LOW);
       delay(LED_DELAY);
     }
-
     counter++;
     delay(1000);
   }
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
+
   if(turn == 0)
     flashGreen();
 
