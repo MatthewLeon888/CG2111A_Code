e183f52625964ad01a4e24e1810e489407e0c70b
Yi An <yiannie726@gmail.com>
2026-01-27 16:14:03 +0800

    working code (cry)

--- Studio 4 (Graded)/activity3_part2/activity3_part2.ino
+++ Studio 4 (Graded)/activity3_part2/activity3_part2.ino
@@ -1,22 +1,28 @@
 #include <avr/io.h>
 #include "Arduino.h"
+#include <avr/interrupt.h>
 
-static volatile bool flag = false;
+
+#define SERVO_SPEED 20 // Vaires the speed of flashing cycle of LED
+static volatile bool counting_up = false; // Variable that toggles between modes
 
 ISR(TIMER1_COMPA_vect) {
-  // When triggered, set servo pin to HIGH
-  PORTD |= PB1;
-  flag = true;
-}
+  if (OCR1A <= 500) {
+    counting_up = true;
+  } else if (OCR1A >= 2800) {
+    counting_up = false;
+  }
 
-ISR(TIMER1_COMPB_vect) {
-  if (flag) {
-    PORTD &= ~PB1;
-    flag = false;
+  if (counting_up) {
+    OCR1A += SERVO_SPEED;
+  } else {
+    OCR1A -= SERVO_SPEED;
   }
 }
 
+
 void setup() {
+  
   cli();
 
   // 1. Set PB1 (Pin 9 on Arduino) as output
@@ -26,23 +32,22 @@ void setup() {
   EICRA |= 0b00000011; // ADDED FOR PART 2
 
   // 2. Set Phase Correct PWM mode
-  TCCR1A = 0b10100011;
+  TCCR1A = 0b10000010;
 
-  TIMSK1 = 0b110; // ADDED FOR PART 2
+  TIMSK1 = 0b10; // ADDED FOR PART 2
 
   // Set Prescaler
   // This has to be set early for Timer1. 
   // By default, the Arduino Wiring language already 
   // sets up Timer1 in a different mode before this, which
   // makes our OCR1A = .. result in a wrong value otherwise  
-  TCCR1B = 0b00010010;
+  TCCR1B = 0b00010010; //prescalar is 8
 
   // 3. Set ICR1
   ICR1 = 20000;
 
   // 4. Set OCR1A for duty cycle
-  OCR1A = 40000;
-  OCR1B = 3000;
+  OCR1A = 1000;
 
   TCNT1 = 0;
 
---
+++ Studio 4 (Graded)/activity3_yes/activity3_yes.ino
@@ -0,0 +1,58 @@
+#include "Arduino.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define PIN7 (1 << 7)
+#define PIN6 (1 << 6)
+#define PIN5 (1 << 5)
+#define PIN4 (1 << 4)
+#define PIN3 (1 << 3)
+#define PIN2 (1 << 2)
+#define PIN1 (1 << 1)
+#define PIN0 (1 << 0)
+
+#define BRIGHTNESS_FACTOR 5 // Varies the brightness range of LED
+#define FLASH_FACTOR 2 // Vaires the speed of flashing cycle of LED
+static volatile bool counting_up = false; // Variable that toggles between modes
+
+void setup() {
+  cli();
+
+  TCCR1A = 0b10000001; // Set OCOM0A to 10 and WGM to 01
+  // Set TCCR0A to 0b11000001 for complement
+
+  TIMSK1 |= 0b10; // Enable Int for Output Compare Match
+
+  OCR1A = 25; // OCR0A can accept from 0 to 255
+
+  TCNT1 = 0;
+
+  TCCR1B = 0b00000100; // Set clk source to clk/64
+  // Increase prescalar to slow down blinking light.
+
+  // Set PORTD Pin 6 (Arduino Pin 6) as Output
+  DDRD |= PIN6;
+
+  sei();
+}
+
+// Warning: if you enable the timer interrupt, you must define
+// the ISR, otherwise your Arduino might continually reset.
+ISR(TIMER0_COMPA_vect) {
+  if (OCR0A <= BRIGHTNESS_FACTOR) {
+    counting_up = true;
+  } else if (OCR0A >= 255 - BRIGHTNESS_FACTOR) {
+    counting_up = false;
+  }
+
+  if (counting_up) {
+    OCR0A += FLASH_FACTOR;
+  } else {
+    OCR0A -= FLASH_FACTOR;
+  }
+}
+
+void loop() {
+  
+}
+
